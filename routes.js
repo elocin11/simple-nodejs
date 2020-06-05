74dc989a0a61de89693b8c120f7c68b309875579
nicole <nicole.alday1111@gmail.com>
2020-06-06 01:40:03 +0800

    initial commit

---
+++ routes.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Nodejs</title></head");
+    res.write(
+      '<body><form action="/post" method="POST"><input type="text" name="message"/><button type="submit">Submit</button></form></body>'
+    );
+    res.write("</html>");
+    res.end();
+  }
+
+  if (url === "/post" && method === "POST") {
+    const body = [];
+
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+
+      fs.writeFile("message.txt", message, (error) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+};
+
+module.exports = requestHandler;
